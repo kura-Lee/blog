c403a383038636e453167be4b0ccecf7b9791267
Ubuntu-Zhong <2499292961@qq.com>
2021-06-09 19:39:59 +0800

    新文件：   .idea/workspace.xml 	重命名：   .idea/.gitignore -> blog/.idea/.gitignore 	重命名：   .idea/blog.iml -> blog/.idea/blog.iml 	重命名：   .idea/inspectionProfiles/profiles_settings.xml -> blog/.idea/inspectionProfiles/profiles_settings.xml 	重命名：   .idea/misc.xml -> blog/.idea/misc.xml 	重命名：   .idea/modules.xml -> blog/.idea/modules.xml 	重命名：   .idea/vcs.xml -> blog/.idea/vcs.xml 	修改：     blog/blog/__pycache__/settings.cpython-38.pyc 	修改：     blog/blog/__pycache__/urls.cpython-38.pyc 	修改：     blog/blog/settings.py 	修改：     blog/blog/urls.py 	新文件：   blog/logs/blog.log

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# 导入系统的logging
+import logging
+# 创建（获取）日志
+logger = logging.getLogger('django')
+
+from django.http import HttpResponse
+def log(request):
+    # 使用日至器记录信息
+    logger.info('info')
+    return HttpResponse("test")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
